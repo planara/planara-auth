ca49d354eeb97eeef8e6304754eeed3f1c1bcad1
a1unade <tags0956@gmail.com>
2026-02-09 18:36:04 +0300

    [fix] redis config for test env

--- src/Program.cs
+++ src/Program.cs
@@ -46,8 +46,13 @@ builder.Services
         _ =>
             ConnectionMultiplexer.Connect(
                 builder.Configuration.GetValue<string>("DbConnections:Redis:ConnectionString")!,
-                c => c.CertificateValidation += (_, _, _, _) => true
-            ),
+                c =>
+                {
+                    c.CertificateValidation += (_, _, _, _) => true;
+                    c.AbortOnConnectFail = !builder.Environment.IsEnvironment("Test");
+                    c.ConnectTimeout = 5000;
+                    c.ReconnectRetryPolicy = new ExponentialRetry(1000);
+                }),
         builder.Configuration.GetValue<string>("GraphQL:Name")!,
         WellKnownSchema.Auth
     )
