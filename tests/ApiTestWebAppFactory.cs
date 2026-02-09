014235f6b055d1e778b4c96967cff341333c6f55
a1unade <tags0956@gmail.com>
2026-02-09 18:15:33 +0300

    [fix] tests

--- tests/ApiTestWebAppFactory.cs
+++ tests/ApiTestWebAppFactory.cs
@@ -2,13 +2,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Planara.Auth.Data;
 using Planara.Common.Kafka;
 using Planara.Kafka.Interfaces;
 using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
 
 namespace Planara.Auth.Tests;
 
@@ -18,6 +18,9 @@ public class ApiTestWebAppFactory: WebApplicationFactory<Program>, IAsyncLifetim
         .WithDatabase("auth-test")
         .WithUsername("postgres")
         .WithPassword("postgres")
+        .Build();
+    
+    private readonly RedisContainer _redis = new RedisBuilder("redis:latest")
         .Build();
     
     protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -43,6 +46,7 @@ public class ApiTestWebAppFactory: WebApplicationFactory<Program>, IAsyncLifetim
     public async Task InitializeAsync()
     {
         await _postgres.StartAsync();
+        await _redis.StartAsync();
 
         using var scope = Services.CreateScope();
         var db = scope.ServiceProvider.GetRequiredService<DataContext>();
@@ -54,5 +58,6 @@ public class ApiTestWebAppFactory: WebApplicationFactory<Program>, IAsyncLifetim
     public new async Task DisposeAsync()
     {
         await _postgres.StopAsync();
+        await _redis.StopAsync();
     }
 }
