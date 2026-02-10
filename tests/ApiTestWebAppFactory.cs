efb7439a2223b65355aa026fb16d7dc08223f176
a1unade <tags0956@gmail.com>
2026-02-10 14:21:31 +0300

    [fix] tests

--- tests/ApiTestWebAppFactory.cs
+++ tests/ApiTestWebAppFactory.cs
@@ -1,17 +1,16 @@
-using AppAny.HotChocolate.FluentValidation;
-using HotChocolate.Types;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Planara.Auth.Data;
-using Planara.Auth.GraphQL;
-using Planara.Common.GraphQL.Filters;
 using Planara.Common.Kafka;
 using Planara.Kafka.Interfaces;
+using StackExchange.Redis;
 using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
 
 namespace Planara.Auth.Tests;
 
@@ -23,6 +22,8 @@ public class ApiTestWebAppFactory: WebApplicationFactory<Program>, IAsyncLifetim
         .WithPassword("postgres")
         .Build();
     
+    private readonly RedisContainer _redis = new RedisBuilder("redis:latest").Build();
+    
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test");
@@ -32,21 +33,7 @@ public class ApiTestWebAppFactory: WebApplicationFactory<Program>, IAsyncLifetim
             services.RemoveAll(typeof(DbContextOptions<DataContext>));
             services.RemoveAll(typeof(DataContext));
             services.RemoveAll(typeof(IKafkaProducer<UserCreatedMessage>));
-
-            services
-                .AddGraphQLServer()
-                .AddErrorFilter<ErrorFilter>()
-                .AddQueryType(m => m.Name(OperationTypeNames.Query))
-                .AddType<Query>()
-                .AddMutationType(m => m.Name(OperationTypeNames.Mutation))
-                .AddType<Mutation>()
-                .AddAuthorization()
-                .AddFluentValidation(options =>
-                {
-                    options.UseInputValidators();
-                    options.UseDefaultErrorMapper();
-                })
-                .InitializeOnStartup();
+            services.RemoveAll(typeof(IConnectionMultiplexer));
 
             services.AddScoped<FakeKafkaProducer>();
             services.AddScoped<IKafkaProducer<UserCreatedMessage>>(sp =>
@@ -55,11 +42,25 @@ public class ApiTestWebAppFactory: WebApplicationFactory<Program>, IAsyncLifetim
             services.AddDbContext<DataContext>(opt =>
                 opt.UseNpgsql(_postgres.GetConnectionString()));
         });
+        
+        builder.ConfigureAppConfiguration((config) =>
+        {
+            config.AddInMemoryCollection(new[]
+            {
+                new KeyValuePair<string, string>(
+                    "DbConnections:Redis:ConnectionString", 
+                    _redis.GetConnectionString()!),
+                new KeyValuePair<string, string>(
+                    "GraphQL:Name", 
+                    "test-auth-schema")
+            }!);
+        });
     }
 
     public async Task InitializeAsync()
     {
         await _postgres.StartAsync();
+        await _redis.StartAsync();
 
         using var scope = Services.CreateScope();
         var db = scope.ServiceProvider.GetRequiredService<DataContext>();
@@ -68,5 +69,9 @@ public class ApiTestWebAppFactory: WebApplicationFactory<Program>, IAsyncLifetim
         await db.Database.MigrateAsync();
     }
 
-    public new async Task DisposeAsync() => await _postgres.StopAsync();
+    public new async Task DisposeAsync()
+    {
+        await _postgres.StopAsync();
+        await _redis.StopAsync();
+    }
 }
