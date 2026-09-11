390f65b0e9799bd5b559d1bb4359e5483a239db7
a1unade <tags0956@gmail.com>
2026-09-11 17:36:07 +0300

    [fix]: date time tests assertion

--- tests/Api/KafkaConsumerWorkerTests.cs
+++ tests/Api/KafkaConsumerWorkerTests.cs
@@ -104,7 +104,7 @@ public class KafkaConsumerWorkerTests: BaseApiTest
         projection.Type.Should().Be(ConsentType.PersonalData);
         projection.ConsentVersionId.Should().Be(newConsentVersionId);
         projection.IsGranted.Should().BeTrue();
-        projection.GrantedAt.Should().Be(givenAt);
+        projection.GrantedAt.Should().BeCloseTo(givenAt, TimeSpan.FromMicroseconds(1));
         projection.RevokedAt.Should().BeNull();
         projection.UpdatedAt.Should().BeOnOrAfter(before);
     }
